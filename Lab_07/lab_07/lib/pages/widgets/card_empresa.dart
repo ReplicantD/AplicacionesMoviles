5c1732b61c86fc4dcd422775555db61cf2ba2639
dani <dgonzalezr@usm.cl>
2025-09-01 12:44:06 -0400

    lab 7.1

--- Lab_07/lab_07/lib/pages/widgets/card_empresa.dart
+++ Lab_07/lab_07/lib/pages/widgets/card_empresa.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
 
 class CardEmpresa extends StatelessWidget {
-  const CardEmpresa({super.key, required this.nombre,
-  required this.icono, required this.sector, required this.mercado, required this.simbolo});
+  const CardEmpresa({
+    super.key,
+    required this.nombre,
+    required this.icono,
+    required this.sector,
+    required this.mercado,
+    required this.simbolo,
+    required this.color,
+  });
 
-  final String nombre,sector, mercado, simbolo;
+  final String nombre, sector, mercado, simbolo;
   final IconData icono;
+  final int color;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: Icon(this.icono),
-      
+      tileColor: Color(this.color),
+      leading: Icon(this.icono, color: Colors.white),
+      title: Text(
+        this.nombre + " " + this.simbolo,
+        style: TextStyle(color: Colors.white),
+      ),
+      subtitle: Text(this.sector, style: TextStyle(color: Colors.white)),
+      trailing: Text(this.mercado, style: TextStyle(color: Colors.white)),
     );
   }
 }
